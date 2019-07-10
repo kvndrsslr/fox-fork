c92066cf01c6eec453695475367d5198e14b0a7d
Kevin Dressler <kvndrsslr@gmail.com>
2019-07-10 05:43:58 +0200

    transfer

--- ScriptRun.sh
+++ ScriptRun.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-export MAVEN_OPTS="-Xmx16G -Dlog4j.configuration=file:data/fox/log4j.properties"
+export MAVEN_OPTS="-Xmx31G -Dlog4j.configuration=file:data/fox/log4j.properties"
 #
 echo "##############################################################################" 
 echo "# Check $0.log file."
 echo "##############################################################################" 
 #
-nohup mvn exec:java  -Dexec.mainClass="org.aksw.fox.ui.FoxRESTful" > $0.log 2>&1 </dev/null &
+mvn exec:java -Dexec.mainClass="org.aksw.fox.ui.FoxRESTful"
---
+++ stop.sh
@@ -0,0 +1,2 @@
+kill 27014
+rm stop.sh
